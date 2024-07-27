5cb6436a5217bb41ba2a09b48a7ca15709b8c145
Fausto Fedele <faustofedele2013@gmail.com>
2024-07-27 01:38:15 -0300

    feat: added tech sphere

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,57 @@
 import Link from "next/link";
-import { ChevronRight } from "./icons/ChevronRight";
+import { ChevronRight } from "../components/icons/ChevronRight";
 import Image from "next/image";
+import PhotoSphere from "@/components/sphere";
 
 export default function Home() {
+  const allTechIconsPaths = [
+    "react.svg",
+    "next.svg",
+    "node.svg",
+    "typescript.svg",
+    "git-hub.svg",
+    "material.svg",
+    "mysql.svg",
+    "aws.svg",
+    "graphql.svg",
+    "docker.svg",
+    "jira.svg",
+    "confluence.svg",
+    "datadog.svg",
+    "jest.svg",
+    "snowflake.svg",
+    "figma.svg",
+    "nest.svg",
+    "android.svg",
+    "ios.svg",
+    "keycloak.svg",
+    "ant-d.svg",
+    "java.svg",
+    "spring.svg",
+    "drive.svg",
+    "mssql.svg",
+    "xd.svg",
+    "gitlab.svg",
+    "google-cloud.svg",
+    "firebase.svg",
+    "linux.svg",
+    "php.svg",
+    "redux.svg",
+    // "wordpress.svg",
+    // "dot-net.svg",
+    // "asana.svg",
+    // "angular.svg",
+    // "jenkins.svg",
+    // "redmine.svg",
+    // "talend.svg",
+    // "trello.svg",
+    // "c-sharp.svg",
+    // "azure.svg",
+  ];
   return (
     <div className="flex flex-col bg-black justify-center">
       <div className="text-center py-4 bg-gray-700">Menu</div>
-      <div className="bg-[url('/picture.jpg')] bg-cover bg-center bg-no-repeat">
+      <div className="bg-[url('/picture.jpg')] bg-cover bg-center bg-no-repeat grayscale hover:filter-none ease-in-out duration-1000">
         <div className="flex flex-col justify-center items-center bg-gradient-to-r from-transparent to-gray-800 px-8 text-gray-300 h-[75vh]">
           <div className="flex w-full flex-col justify-center items-end">
             <p className="font-thin text-xl xl:text-2xl">I&apos;m</p>
@@ -47,318 +92,9 @@ export default function Home() {
             <p className="text-sm font-thin">Projects</p>
           </div>
         </div>
-        <div
-          className="h-[800px] w-full my-5"
-          style={{
-            background: "radial-gradient(farthest-side, #fff, transparent)",
-          }}
-        >
-          <div className="flex flex-col justify-center items-center h-[650px] py-10">
-            <div className="flex justify-center items-center flex-1 gap-3">
-              <Image
-                unoptimized
-                alt="react-logo"
-                width={125}
-                height={125}
-                src="react.svg"
-              />
-              <Image
-                unoptimized
-                alt="next-logo"
-                width={125}
-                height={125}
-                src="next.svg"
-              />
-              <Image
-                unoptimized
-                alt="node-logo"
-                width={125}
-                height={125}
-                src="node.svg"
-              />
-              <Image
-                unoptimized
-                alt="typescript-logo"
-                width={115}
-                height={115}
-                src="typescript.svg"
-              />
-              <Image
-                unoptimized
-                alt="git-hub-logo"
-                width={125}
-                height={125}
-                src="git-hub.svg"
-              />
-              <Image
-                unoptimized
-                alt="material-logo"
-                width={125}
-                height={125}
-                src="material.svg"
-              />
-              <Image
-                unoptimized
-                alt="mysql-logo"
-                width={160}
-                height={160}
-                src="mysql.svg"
-              />
-            </div>
-            <div className="flex justify-center items-center flex-1 gap-3">
-              <Image
-                unoptimized
-                alt="aws-logo"
-                width={100}
-                height={100}
-                src="aws.svg"
-              />
-              <Image
-                unoptimized
-                alt="graphql-logo"
-                width={100}
-                height={100}
-                src="graphql.svg"
-              />
-              <Image
-                unoptimized
-                alt="docker-logo"
-                width={100}
-                height={100}
-                src="docker.svg"
-              />
-              <Image
-                unoptimized
-                alt="jira-logo"
-                width={100}
-                height={100}
-                src="jira.svg"
-              />
-              <Image
-                unoptimized
-                alt="confluence-logo"
-                width={100}
-                height={100}
-                src="confluence.svg"
-              />
-              <Image
-                unoptimized
-                alt="datadog-logo"
-                width={100}
-                height={100}
-                src="datadog.svg"
-              />
-              <Image
-                unoptimized
-                alt="jest-logo"
-                width={90}
-                height={90}
-                src="jest.svg"
-              />
-              <Image
-                unoptimized
-                alt="snowflake-logo"
-                width={100}
-                height={100}
-                src="snowflake.svg"
-              />
-              <Image
-                unoptimized
-                alt="figma-logo"
-                width={100}
-                height={100}
-                src="figma.svg"
-              />
-            </div>
-            <div className="flex justify-center items-center flex-1 gap-3">
-              <Image
-                unoptimized
-                alt="nest-logo"
-                width={80}
-                height={80}
-                src="nest.svg"
-              />
-              <Image
-                unoptimized
-                alt="android-logo"
-                width={80}
-                height={80}
-                src="android.svg"
-              />
-              <Image
-                unoptimized
-                alt="ios-logo"
-                width={80}
-                height={80}
-                src="ios.svg"
-              />
-              <Image
-                unoptimized
-                alt="keycloak-logo"
-                width={80}
-                height={80}
-                src="keycloak.svg"
-              />
-              <Image
-                unoptimized
-                alt="ant-d-logo"
-                width={70}
-                height={70}
-                src="ant-d.svg"
-              />
-              <Image
-                unoptimized
-                alt="java-logo"
-                width={80}
-                height={80}
-                src="java.svg"
-              />
-              <Image
-                unoptimized
-                alt="spring-logo"
-                width={70}
-                height={70}
-                src="spring.svg"
-              />
-              <Image
-                unoptimized
-                alt="drive-logo"
-                width={80}
-                height={80}
-                src="drive.svg"
-              />
-              <Image
-                unoptimized
-                alt="mssql-logo"
-                width={80}
-                height={80}
-                src="mssql.svg"
-              />
-              <Image
-                unoptimized
-                alt="xd-logo"
-                width={70}
-                height={70}
-                src="xd.svg"
-              />
-              <Image
-                unoptimized
-                alt="gitlab-logo"
-                width={80}
-                height={80}
-                src="gitlab.svg"
-              />
-            </div>
-            <div className="flex justify-center items-center flex-1 gap-3">
-              <Image
-                unoptimized
-                alt="google-cloud-logo"
-                width={50}
-                height={50}
-                src="google-cloud.svg"
-              />
-              <Image
-                unoptimized
-                alt="firebase-logo"
-                width={50}
-                height={50}
-                src="firebase.svg"
-              />
-              <Image
-                unoptimized
-                alt="linux-logo"
-                width={50}
-                height={50}
-                src="linux.svg"
-              />
-              <Image
-                unoptimized
-                alt="php-logo"
-                width={50}
-                height={50}
-                src="php.svg"
-              />
-              <Image
-                unoptimized
-                alt="redux-logo"
-                width={50}
-                height={50}
-                src="redux.svg"
-              />
-              <Image
-                unoptimized
-                alt="wordpress-logo"
-                width={50}
-                height={50}
-                src="wordpress.svg"
-              />
-              <Image
-                unoptimized
-                alt="dot-net-logo"
-                width={50}
-                height={50}
-                src="dot-net.svg"
-              />
-              <Image
-                unoptimized
-                alt="asana-logo"
-                width={50}
-                height={50}
-                src="asana.svg"
-              />
-              <Image
-                unoptimized
-                alt="angular-logo"
-                width={50}
-                height={50}
-                src="angular.svg"
-              />
-              <Image
-                unoptimized
-                alt="jenkins-logo"
-                width={50}
-                height={50}
-                src="jenkins.svg"
-              />
-              <Image
-                unoptimized
-                alt="redmine-logo"
-                width={75}
-                height={75}
-                src="redmine.svg"
-              />
-              <Image
-                unoptimized
-                alt="talend-logo"
-                width={50}
-                height={50}
-                src="talend.svg"
-                className="ml-[-20px]"
-              />
-              <Image
-                unoptimized
-                alt="trello-logo"
-                width={50}
-                height={50}
-                src="trello.svg"
-              />
-              <Image
-                unoptimized
-                alt="c-sharp-logo"
-                width={50}
-                height={50}
-                src="c-sharp.svg"
-              />
-              <Image
-                unoptimized
-                alt="azure-logo"
-                width={60}
-                height={60}
-                src="azure.svg"
-              />
-            </div>
-          </div>
-        </div>
+      </div>
+      <div className="w-[100%]">
+        <PhotoSphere imageData={allTechIconsPaths} />
       </div>
       <div className="mt-[100px] text-white">
         <h2 id="about-me">About me</h2>
---
+++ src/components/sphere/index.tsx
@@ -0,0 +1,319 @@
+"use client";
+import { SyntheticEvent, useEffect, useRef } from "react";
+
+const CENTER_COUNT = 12;
+const RADIUS = 600;
+const TOTAL_ROW = 3;
+
+const PhotoSphere = (props: { imageData: string[] }) => {
+  const el = useRef<HTMLDivElement>(null);
+  const img = useRef<HTMLDivElement>(null);
+  const container = useRef<HTMLDivElement>(null);
+  // Update animation
+  const updateFrame = (mouseShift: number) => {
+    const items = el.current!.children;
+    if (items.length == 0) return;
+
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i] as HTMLDivElement;
+      if (item.style.backgroundImage === 'url("spring.svg")') {
+        // debugger;
+      }
+      const prevAngle = parseFloat(item.dataset.angle!);
+      const ypos = parseFloat(item.dataset.ypos!);
+      const radius = parseFloat(item.dataset.radius!);
+      const prevXp = parseFloat(item.dataset.xp!);
+      const prevZp = parseFloat(item.dataset.zp!);
+      const percentage = (mouseShift * 100) / RADIUS;
+      const newMouseShift = radius * (percentage / 100);
+      const angle = prevAngle + Math.asin(newMouseShift / radius);
+      const newXp = Math.sin(angle) * radius;
+      const newZp = Math.cos(angle) * radius;
+      // let diff;
+      // if (prevZp > 0) {
+      //   diff = prevXp - newMouseShift;
+      // } else {
+      //   diff = prevXp + newMouseShift;
+      // }
+      // let changeZSign = false;
+      // let newXp;
+      // if (Math.abs(diff) < radius) {
+      //   newXp = diff;
+      // } else {
+      //   changeZSign = true;
+      //   if (diff > 0) {
+      //     newXp = radius - (diff % radius);
+      //   } else {
+      //     newXp = -radius + (Math.abs(diff) % radius);
+      //   }
+      // }
+      // let newAngle = Math.asin(newXp / radius);
+      // [
+      //   'url("node.svg")',
+      //   'url("typescript.svg")',
+      //   'url("git-hub.svg")',
+      // ].includes(item.style.backgroundImage) &&
+      //   console.log("(prevXp - mouseShift) % radius", diff % radius);
+      // // while (newAngle < 0) {
+      // //   newAngle += 2 * Math.PI;
+      // // }
+      // // while (newAngle > 2 * Math.PI) {
+      // //   newAngle -= 2 * Math.PI;
+      // // }
+      // let newZp = Math.cos(newAngle) * radius;
+      // if (prevZp < 0) {
+      //   newZp = -newZp;
+      // }
+      // if (changeZSign) {
+      //   newZp = -newZp;
+      // }
+      // [
+      //   'url("node.svg")',
+      //   'url("typescript.svg")',
+      //   'url("git-hub.svg")',
+      // ].includes(item.style.backgroundImage) &&
+      //   console.log("newAngle", {
+      //     newAngle,
+      //     mouseShift: newMouseShift,
+      //     radius,
+      //     prevXp,
+      //     newXp,
+      //     newZp,
+      //     prevZp,
+      //     asd: diff,
+      //     changeZSign,
+      //     i,
+      //   });
+      // if ([0, 30].includes(i)) {
+      //   // prevXp = Math.cos(angleRad) * radius;
+      //   const supposedAngle = Math.asin(prevXp / radius);
+      //   const alpha = 0;
+      //   const beta = 0;
+      //   // console.log("log!", {
+      //   //   i,
+      //   //   gamma,
+      //   //   beta,
+      //   //   beta2,
+      //   // });
+      //   console.log("log!", {
+      //     i,
+      //     prevXp,
+      //     mouseShift,
+      //     angle,
+      //     supposedAngle,
+      //     supposedAngle2: Math.acos(prevZp / radius),
+      //     newXp,
+      //     newAngle,
+      //   });
+      // }
+      // angle += mouseShift;
+      item.dataset.angle = angle.toString();
+      item.dataset.xp = newXp.toString();
+      item.dataset.zp = newZp.toString();
+      item.style.transform = `translateY(${ypos}px) translateX(${newXp}px) translateZ(${newZp}px)`;
+    }
+  };
+
+  useEffect(() => {
+    // console.log(props.imageData);
+
+    let listen = false;
+    let starting = 0;
+    let timeout: NodeJS.Timeout;
+    let maxValueMoved = 0;
+
+    const containerRef = container.current;
+
+    const downListener = (e: MouseEvent) => {
+      listen = true;
+      starting = e.clientX;
+    };
+    const moveListener = (e: MouseEvent) => {
+      const moved = starting - e.clientX;
+      const movedAbs = Math.abs(moved);
+      if (listen && movedAbs > 5 && movedAbs < 500) {
+        if (Math.abs(moved) > maxValueMoved) {
+          maxValueMoved = Math.abs(moved);
+          console.log("maxValueMoved", maxValueMoved);
+        }
+        timeout && clearTimeout(timeout);
+        timeout = setTimeout(() => {
+          starting = e.clientX;
+        }, 50);
+        // console.log("starting", {
+        //   starting,
+        //   clientX: e.clientX,
+        //   move: e.movementX,
+        //   page: e.pageX,
+        //   containerWidth,
+        // });
+        updateFrame(starting - e.clientX);
+      }
+    };
+    const upListener = (e: MouseEvent) => {
+      listen = false;
+    };
+    if (props.imageData) {
+      const items = el.current!.children;
+      if (items.length == 0) return;
+
+      // Setup and distribute items in correct positions
+      const angleDivision = ((90 / TOTAL_ROW) * Math.PI) / 180;
+      // const angleDivision = 0,523598775598299;
+      let radius,
+        ypos,
+        rowItemsCount = CENTER_COUNT;
+      let indexCount = 0;
+
+      for (let k = 0; k < TOTAL_ROW; k++) {
+        radius = RADIUS * Math.cos(k * angleDivision);
+        ypos = RADIUS * Math.sin(-k * angleDivision);
+        // console.log("log", {
+        //   ypos,
+        //   angleDivision,
+        //   RADIUS,
+        //   radius,
+        //   k,
+        //   sin: Math.sin(-k * angleDivision),
+        // });
+        rowItemsCount = CENTER_COUNT / (k + 1);
+
+        let angleUnit = 360 / rowItemsCount;
+        // console.log("k", {
+        //   k,
+        //   angle: k * (90 / TOTAL_ROW),
+        //   rowItemsCount,
+        //   angleUnit,
+        // });
+
+        for (let i = 0; i < rowItemsCount; i++) {
+          let angleRad = (angleUnit * i * Math.PI) / 180;
+          do {
+            angleRad -= 2 * Math.PI;
+          } while (angleRad > 2 * Math.PI);
+          const xp = Math.sin(angleRad) * radius;
+          const zp = Math.cos(angleRad) * radius;
+
+          // Upper item
+          const item = items[indexCount] as HTMLDivElement;
+          if ([0, 1, 2, 3, 4, 5, 12, 7, 30].includes(indexCount)) {
+            console.log("log prev", {
+              i,
+              angleUnit,
+              xp,
+              angleRad,
+              radius,
+              supposedAngle: Math.asin(xp / radius),
+              supposedAngle2: Math.acos(zp / radius),
+              zp,
+              item: item.style.backgroundImage,
+            });
+          }
+          // console.log("i", {
+          //   i,
+          //   angleDeg: angleUnit * i,
+          //   item: item.style.backgroundImage,
+          //   ypos: ypos === 0 ? ypos : -ypos,
+          //   xp,
+          //   zp,
+          // });
+          item.className = "photosphere-item";
+          item.style.transform = `translateY(${ypos}px) translateX(${xp}px) translateZ(${zp}px)`;
+          indexCount++;
+          if ([0, 12, 7, 30].includes(indexCount)) {
+            console.log("log prev", {
+              i,
+              angleUnit,
+              xp,
+              angleRad,
+              radius,
+              supposedAngle: Math.asin(xp / radius),
+              supposedAngle2: Math.acos(zp / radius),
+              zp,
+              item: (items[indexCount] as HTMLDivElement).style.backgroundImage,
+            });
+          }
+          // Save item data
+          item.dataset.radius = radius.toString();
+          item.dataset.angle = angleRad.toString();
+          item.dataset.ypos = ypos.toString();
+          item.dataset.xp = xp.toString();
+          item.dataset.zp = zp.toString();
+
+          if (k !== 0) {
+            // Lower item
+            const item2 = items[indexCount] as HTMLDivElement;
+            // console.log("i", {
+            //   i,
+            //   angleDeg: angleUnit * i,
+            //   item: item2.style.backgroundImage,
+            //   ypos: ypos === 0 ? ypos : -ypos,
+            //   xp,
+            //   zp,
+            // });
+            item2.className = "photosphere-item";
+            item2.style.transform = `translateY(${-ypos}px) translateX(${xp}px) translateZ(${zp}px)`;
+            indexCount++;
+            item2.dataset.radius = radius.toString();
+            item2.dataset.angle = angleRad.toString();
+            item2.dataset.ypos = (-ypos).toString();
+            item2.dataset.xp = xp.toString();
+            item2.dataset.zp = zp.toString();
+          }
+        }
+      }
+
+      // updateFrame(0);
+
+      containerRef?.addEventListener("mousedown", downListener);
+      window.addEventListener("mousemove", moveListener, true);
+      window.addEventListener("mouseup", upListener);
+      window.addEventListener("resize", () => {
+        if (el.current) {
+          console.log(
+            "el.current.style.transition",
+            el.current.style.transition
+          );
+          el.current.style.transform = `translateZ(min(-${
+            4000 - innerWidth * 2
+          }px, -1200px))`;
+          // el.current.style.height = `${innerWidth / 50}px`;
+          console.log(
+            "el.current.style.transition after",
+            el.current.style.transition
+          );
+        }
+      });
+
+      // Calculate mouse shift
+      // const onMouseMove = (e: MouseEvent) => {
+      //   console.log("e.offsetX", e.offsetX);
+      //   // console.log("e", e);
+      //   // console.log("innerWidth", innerWidth);
+      //   mouseShift = 0;
+      // };
+      // el.current?.addEventListener("drag", onMouseMove);
+    }
+    return () => {
+      window.removeEventListener("mouseup", upListener);
+      window.removeEventListener("mousemove", moveListener);
+      containerRef?.removeEventListener("mousedown", downListener);
+    };
+  }, [props.imageData]);
+
+  return (
+    <div className="container my-4" ref={container}>
+      <div className="photosphere" ref={el}>
+        {props.imageData.map((it, index) => (
+          <div
+            key={index}
+            style={{ backgroundImage: `url(${it})` }}
+            className="photosphere-item"
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PhotoSphere;
