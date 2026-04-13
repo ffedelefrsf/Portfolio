c17eaa1298b384cdc34edff072b6076ad1bc3021
Fausto Fedele <faustofedele2013@gmail.com>
2026-04-12 23:22:55 -0300

    chore: disabled next image optimization

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true,
+  },
   async redirects() {
     return [
       {
